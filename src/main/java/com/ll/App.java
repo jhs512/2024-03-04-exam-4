3031a3ae067ec4ebf4f392d37cf4d7f9acb87356
jhs512 <jangka512@gmail.com>
2024-03-04 22:18:16 +0900

    work

---
+++ src/main/java/com/ll/App.java
@@ -0,0 +1,16 @@
+package com.ll;
+
+public class App {
+    public void run() {
+        System.out.println("계산기, 시작");
+
+        int num1 = 10;
+        int num2 = 20;
+
+        int rs = num1 + num2;
+
+        System.out.println("rs : " + rs);
+
+        System.out.println("계산기, 끝");
+    }
+}
